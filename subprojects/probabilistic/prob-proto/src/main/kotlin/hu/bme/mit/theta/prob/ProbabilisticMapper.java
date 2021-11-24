83f3f78829e7da657eaa57fdf8137b9c43247e31
Levente Bajczi <bajczilevi@gmail.com>
2021-11-24 17:39:07 +0100

    Added coin implementation

--- subprojects/probabilistic/prob-proto/src/main/kotlin/hu/bme/mit/theta/prob/ProbabilisticMapper.java
+++ subprojects/probabilistic/prob-proto/src/main/kotlin/hu/bme/mit/theta/prob/ProbabilisticMapper.java
@@ -1,11 +1,84 @@
 package hu.bme.mit.theta.prob;
 
+import com.ibm.icu.impl.Pair;
+import hu.bme.mit.theta.core.decl.VarDecl;
+import hu.bme.mit.theta.core.type.Expr;
+import hu.bme.mit.theta.core.type.anytype.RefExpr;
+import hu.bme.mit.theta.core.type.inttype.IntType;
+import hu.bme.mit.theta.frontend.transformation.grammar.expression.Reference;
+import hu.bme.mit.theta.xcfa.model.XcfaEdge;
+import hu.bme.mit.theta.xcfa.model.XcfaLabel;
 import hu.bme.mit.theta.xcfa.model.XcfaProcedure;
+import hu.bme.mit.theta.xcfa.passes.procedurepass.FpFunctionsToExprs;
 import hu.bme.mit.theta.xcfa.passes.procedurepass.ProcedurePass;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.Preconditions.checkState;
+import static hu.bme.mit.theta.core.stmt.Stmts.Assign;
+import static hu.bme.mit.theta.core.stmt.Stmts.Skip;
+import static hu.bme.mit.theta.core.type.abstracttype.AbstractExprs.Add;
+import static hu.bme.mit.theta.core.type.abstracttype.AbstractExprs.Div;
+import static hu.bme.mit.theta.core.type.inttype.IntExprs.Int;
+import static hu.bme.mit.theta.core.utils.TypeUtils.cast;
+import static hu.bme.mit.theta.xcfa.model.XcfaLabel.Stmt;
+
 public class ProbabilisticMapper extends ProcedurePass {
+	private static final Map<String, BiFunction<XcfaProcedure.Builder, XcfaLabel.ProcedureCallXcfaLabel, XcfaLabel>> handlers = new LinkedHashMap<>();
+	private static void addHandler(String[] names, BiFunction<XcfaProcedure.Builder, XcfaLabel.ProcedureCallXcfaLabel, XcfaLabel> handler) {
+		for (String name : names) {
+			handlers.put(name, handler);
+		}
+	}
+	static {
+		addHandler(new String[]{"coin"}, ProbabilisticMapper::handleCoin);
+		addHandler(new String[]{"uniform"}, ProbabilisticMapper::handleUniform);
+	}
+
+	private static XcfaLabel handleCoin(XcfaProcedure.Builder builder, XcfaLabel.ProcedureCallXcfaLabel label) {
+		final List<Expr<?>> params = label.getParams();
+		checkState(params.get(0) instanceof RefExpr, "Return param must be a reference!");
+		final VarDecl<?> varGeneric = (VarDecl<?>) ((RefExpr<?>) params.get(0)).getDecl();
+		final VarDecl<IntType> var = cast(varGeneric, Int());
+		final Expr<?> a = params.get(1);
+		final Expr<?> b = params.get(2);
+		final Expr<?> sum = Add(a, b);
+		return Stmt(new ProbStmt(new EnumeratedDistribution(List.of(
+				Pair.of(Assign(var, Int(0)), Div(a, sum)),
+				Pair.of(Assign(var, Int(1)), Div(b, sum))
+		))));
+	}
+
+
+	private static XcfaLabel handleUniform(XcfaProcedure.Builder builder, XcfaLabel.ProcedureCallXcfaLabel label) {
+		// TODO
+		return Stmt(Skip());
+	}
+
 	@Override
 	public XcfaProcedure.Builder run(XcfaProcedure.Builder builder) {
-		return null;
+		for (XcfaEdge edge : new ArrayList<>(builder.getEdges())) {
+			List<XcfaLabel> newStmts = new ArrayList<>();
+			boolean found = false;
+			for (XcfaLabel stmt : edge.getLabels()) {
+				if(stmt instanceof XcfaLabel.ProcedureCallXcfaLabel) {
+					if(handlers.containsKey(((XcfaLabel.ProcedureCallXcfaLabel) stmt).getProcedure())) {
+						newStmts.add(handlers.get(((XcfaLabel.ProcedureCallXcfaLabel) stmt).getProcedure()).apply(builder, (XcfaLabel.ProcedureCallXcfaLabel) stmt));
+						found = true;
+					}
+				}
+				else newStmts.add(stmt);
+			}
+			if(found) {
+				builder.removeEdge(edge);
+				builder.addEdge(XcfaEdge.of(edge.getSource(), edge.getTarget(), newStmts));
+			}
+		}
+		return builder;
 	}
 }
